0a3b8cb1f744db9818f7513c3a9da2d20d11ffee
la_foca <anngor22@gmail.com>
2019-11-18 11:31:08 +0300

    processor init

---
+++ scorekit/processor/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .base import MissingProcessor, StabilityAnalyzer, DataVisualizer, TargetTrendVisualizer, CorrelationAnalyzer, VIF, FeatureEncoder, GiniChecker, BusinessLogicChecker, WOEOrderChecker, WaldBSChecker, FullnessAnalyzer
+
+__all__ = ['MissingProcessor', 'StabilityAnalyzer', 'DataVisualizer', 'TargetTrendVisualizer', 'CorrelationAnalyzer', 'VIF', 'FeatureEncoder', 'GiniChecker',
+           'BusinessLogicChecker', 'WOEOrderChecker', 'WaldBSChecker', 'FullnessAnalyzer']

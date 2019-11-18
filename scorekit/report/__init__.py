53685412e2c4ae3a517da14806f34dad032c673d
la_foca <anngor22@gmail.com>
2019-11-18 11:35:27 +0300

    report init

---
+++ scorekit/report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .base import Title, Introduction, DataCollection, DataProcessing, OneFactor, MultiFactor, Stability, ModelReport, Report
+
+__all__ = ['Title', 'Introduction', 'DataCollection', 'DataProcessing', 'OneFactor',
+           'MultiFactor', 'Stability', 'ModelReport', 'Report']
